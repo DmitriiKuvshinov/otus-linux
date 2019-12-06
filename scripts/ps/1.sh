6aec75491d93baad85c1e894501174e479a6e233
Kuvshinov Dmitriy <kuvshinov@embria.com>
2019-12-06 09:59:26 +0300

    fix some bugs

--- scripts/ps/1.sh
+++ scripts/ps/1.sh
@@ -6,15 +6,21 @@ pid_list=$(ls /proc/ | grep -E '^[0-9]+' | sort -n)
 clk_tck_cpu=$(getconf CLK_TCK)
 for i in ${pid_list[@]}
 do
-    stat=$(cat /proc/$i/stat);
-    cmd=$(cat /proc/$i/cmdline 2>/dev/null);
-    state=$(cat /proc/$i/status | grep State | awk {'print $2'});
+    stat=$(cat /proc/$i/stat 2>/dev/null);
+    cmd=$(cat /proc/$i/status 2>/dev/null | grep Name | awk {'print $2'});
+    cmd_f=$(cat /proc/$i/cmdline 2>/dev/null);
+    state=$(cat /proc/$i/status 2>/dev/null | grep State | awk {'print $2'});
+
     pid=$(echo $stat | awk '{print $1}' 2>/dev/null);
     tty=$(echo $stat | awk '{print $7}' 2>/dev/null);
     utime=$(echo $stat | awk '{print $13}' 2>/dev/null);
     stime=$(echo $stat | awk '{print $14}' 2>/dev/null);
+
     pid_time=0
-    pid_time=$(echo $utime / $clk_tck_cpu + $stime / $clk_tck_cpu | bc);
+    pid_time=$(echo $utime / $clk_tck_cpu + $stime / $clk_tck_cpu | bc 2>/dev/null);
+    tty_of_proc=$(echo $stat | awk '{print $7}' 2>/dev/null);
+    tty_of_proc_binary=$(echo "obase=2;$tty_of_proc" | bc 2>/dev/null)
+
     tty_min_bin=$(echo $tty_of_proc_binary | grep -o '[0-1]\{8\}$')
     tty_maj_bin=$(echo $tty_of_proc_binary | sed "s/[0-1]\{8\}$//g")
     tty_min_dec=$((2#$tty_min_bin))
@@ -27,7 +33,7 @@ do
     if [[ -z $result_tty ]]; then
         result_tty='?'
     fi
-    echo $i " " $result_tty " " $state " "  $pid_time " " $cmd >> $ps_file;
+    echo "$i $result_tty $state  $pid_time  [$cmd $cmd_f]"? >> $ps_file;
 done
 
-cat $ps_file | column -t
+cat $ps_file
